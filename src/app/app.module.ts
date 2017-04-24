b6496d458d48ea774352a5e6106f91ca79e4c197
Tyler Saballus <tysabs@gmail.com>
2017-04-24 17:48:56 -0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+import { MainNavComponent } from './components/main-nav/main-nav.component';
+
+import { HomeContentComponent } from './components/home-content/home-content.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeContentComponent }
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    HomeContentComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
