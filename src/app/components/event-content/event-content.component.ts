cd802ab3e5a9c91f6ed8d5903b2f140774752950
Tyler Saballus <tysabs@gmail.com>
2017-05-02 23:06:33 -0500

    Change image alignment to use css flex styles

--- src/app/components/event-content/event-content.component.ts
+++ src/app/components/event-content/event-content.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'event-content',
   templateUrl: './event-content.component.html',
-  styleUrls: ['../two-col-styles.css', './event-content.component.css']
+  styleUrls: ['./event-content.component.css']
 })
 export class EventContentComponent {
   title = "Events Works!"
