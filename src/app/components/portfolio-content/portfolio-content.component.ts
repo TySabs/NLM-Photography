a0dd763be939e282303b8a663f3a13c74eb2d407
Tyler Saballus <tysabs@gmail.com>
2017-05-07 18:07:51 -0500

    Add reviews to the-experience page

--- src/app/components/portfolio-content/portfolio-content.component.ts
+++ src/app/components/portfolio-content/portfolio-content.component.ts
@@ -14,15 +14,16 @@ export class PortfolioContentComponent {
   gallery: GalleryImage[] = [];
 
   constructor(public scrollToTopService: ScrollToTopService) {
-    for (let i = 1; i < 81; i++) {
+    for (let i = 1; i < 101; i++) {
       const smImgString: string = "./assets/portfolio/portfolio-" + i.toString() + ".jpg";
       const lgImgString: string = "./assets/portfolio/large/portfolio-" + i.toString() + "-lg.jpg";
-
       const currentImage = {
         smUrl: smImgString,
         lgUrl: lgImgString
-      }
+      };
+
       this.gallery.push(currentImage);
     }
   }
+
 }
